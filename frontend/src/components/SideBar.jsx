7a40eeccb5efc396b6cf752eefcffad870ebe95e
jademon <j2python@gmail.com>
2024-01-02 15:55:13 +0900

    add sidebar seemore

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import routes from "../routes";
 
+import logo from "../assets/logo.svg";
+import SeemoreSVG from "../assets/seemore.svg";
+
 function SideBar() {
   const location = useLocation();
   const pathname = location.pathname;
@@ -22,19 +25,35 @@ function SideBar() {
       </div>
     </div>
   ) : (
-    <div className="w-[300px]">
-      <h1 className="text-3xl font-bold underline text-red">로고1</h1>
-      {routes.map(
-        (v, i) =>
-          v.menu && (
-            <Link to={"/"} key={i} className="flex d-flex p-[12px] my-[4px]">
-              <div className="w-[24px] h-[24px]">
-                <img src={v.img} alt={v.engName} />
-              </div>
-              <div className="pl-[16px]">{v.korName}</div>
-            </Link>
-          )
-      )}
+    <div className="sticky top-0 h-[100vh] w-[300px] pt-[8px] pb-[20px] px-[12px]">
+      <div className="pt-[25px] pb-[16px] px-[12px] mb-[19px]">
+        <img src={logo} alt={"clone-star"} />
+      </div>
+      <div className="h-[calc(100%-77px-28px)] flex flex-col justify-between">
+        <div>
+          {routes.map(
+            (v, i) =>
+              v.menu && (
+                <Link
+                  to={"/"}
+                  key={i}
+                  className="flex d-flex p-[12px] my-[4px]"
+                >
+                  <div className="w-[24px] h-[24px]">
+                    <img src={v.img} alt={v.engName} />
+                  </div>
+                  <div className="pl-[16px]">{v.korName}</div>
+                </Link>
+              )
+          )}
+        </div>
+        <Link to={"/"} className="flex d-flex p-[12px] my-[4px]">
+          <div className="w-[24px] h-[24px]">
+            <img src={SeemoreSVG} alt={"see more"} />
+          </div>
+          <div className="pl-[16px]">{"더 보기"}</div>
+        </Link>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Mypage.jsx
+++ frontend/src/pages/Mypage.jsx
@@ -1,8 +1,8 @@
 function Mypage() {
   return (
-    <>
+    <div className="h-[2000px]">
       <h1 className="text-3xl font-bold underline text-red">Mypage</h1>
-    </>
+    </div>
   );
 }
 
