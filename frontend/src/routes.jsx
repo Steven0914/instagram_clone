5a40239b1b145ac4ad3841d9366b15607b2e70fb
jademon <j2python@gmail.com>
2024-01-03 23:27:08 +0900

    add postDetail(#5)

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -2,6 +2,7 @@ import Mypage from "./pages/Mypage";
 import NotFound from "./pages/NotFound";
 
 import HomeSVG from "./assets/home.svg";
+import PostDetail from "./pages/PostDetail";
 
 const routes = [
   {
@@ -19,6 +20,13 @@ const routes = [
     korName: "프로필",
     img: HomeSVG,
     menu: true,
+  },
+  {
+    path: "/posts/:id",
+    element: <PostDetail />,
+    engName: "postDetail",
+    korName: "게시글 디테일",
+    img: HomeSVG,
   },
   {
     path: "*",
