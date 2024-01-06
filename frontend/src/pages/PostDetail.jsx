93fefd61f0c3fe36a62a94e2b85e941bff2119a9
jademon <j2python@gmail.com>
2024-01-06 23:42:45 +0900

    update x-btn

--- frontend/src/pages/PostDetail.jsx
+++ frontend/src/pages/PostDetail.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import Mypage from "./Mypage";
 import Modal from "../components/Modal";
 
+import XBtnSVG from "../assets/x-btn.svg";
+
 function PostDetail() {
   const [modal, setModal] = useState(true);
 
@@ -17,10 +19,14 @@ function PostDetail() {
     <>
       <Modal setModal={setModal} maxWidth={"1562px"} maxHeight={"950px"}>
         <div
-          className="fixed top-10 right-10 text-white w-[24px] h-[24px] flex justify-center items-center"
+          className="fixed top-10 right-10 text-white w-[24px] h-[24px] flex justify-center items-center cursor-pointer"
           onClick={() => setModal(false)}
         >
-          X
+          <img
+            src={XBtnSVG}
+            alt={"cancle"}
+            className="w-[18px] h-[18px] mr-[6px]"
+          />
         </div>
         <div className=" h-full max-h-[950px] w-[calc(100vw-48px)] max-w-[1562px] flex">
           <div className="bg-white flex items-center max-w-[950px] min-w-[500px] w-[calc(100vh-48px)]">
